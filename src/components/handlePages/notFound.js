31bea569b5778a7058e87b58815ab8044a96abae
hamza <tamarihamza4@gmail.com>
2024-06-29 16:35:06 +0300

    finishs

--- src/components/handlePages/notFound.js
+++ src/components/handlePages/notFound.js
@@ -1,28 +1,46 @@
+import { useState } from 'react';
 import './notFound.scss'
-export default function Notfound(){
-return (
-    <section class="errorPage">
-    <main class="error-page">
-  <div class="containerError">
-    <div class="eyes">
-      <div class="eye">
-        <div class="eye__pupil eye__pupil--left"></div>
+export default function Notfound() {
+  const [currentTheme, setCurrentTheme] = useState("light");
+
+  const toggleTheme = () => {
+    const page = document.getElementById('errorPage');
+
+    if (currentTheme === "dark") {
+      page.style.setProperty("--bg-color", "#fff");
+      page.style.setProperty("--text-color", "#000");
+      setCurrentTheme("light");
+    } else {
+      page.style.setProperty("--bg-color", "#050505");
+      page.style.setProperty("--text-color", "#fff");
+      setCurrentTheme("dark");
+    }
+  };
+  return (
+    <section id="errorPage" className="errorPage">
+    <main className="error-page">
+      <div className="containerError">
+        <div className="eyes">
+          <div className="eye">
+            <div className="eye__pupil eye__pupil--left"></div>
+          </div>
+          <div className="eye">
+            <div className="eye__pupil eye__pupil--right"></div>
+          </div>
+        </div>
+
+        <div className="error-page__heading">
+          <h1 className="error-page__heading-title">Looks like you're lost</h1>
+          <p className="error-page__heading-desciption">404 error</p>
+        </div>
+
+        <a className="error-page__button" href="/" aria-label="back to home" title="back to home">back to home</a>
       </div>
-      <div class="eye">
-        <div class="eye__pupil eye__pupil--right"></div>
-      </div>
-    </div>
+    </main>
 
-    <div class="error-page__heading">
-      <h1 class="error-page__heading-title">Looks like you're lost</h1>
-      <p class="error-page__heading-desciption">404 error</p>
-    </div>
-
-    <a class="error-page__button" href="/" aria-label="back to home" title="back to home" >back to home</a>
-  </div>
-</main>
-
-<button class="color-switcher" data-theme-color-switch>&#127769;</button>
-    </section>
-)
+    <button className="color-switcher" data-theme-color-switch onClick={toggleTheme}>
+      {currentTheme === "dark" ? "\u{1F319}" : "\u{2600}"}
+    </button>
+  </section>
+  )
 }
